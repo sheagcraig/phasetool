0c171858071afa0947bc6860aa19ec31558359e3
Shea Craig <sheagcraig@gmail.com>
2015-11-02 20:38:46 -0500

    Add actual file manipulation test.

--- test/test_acceptance.py
+++ test/test_acceptance.py
@@ -1,8 +1,15 @@
+import datetime
+import os
+import plistlib
 import subprocess
+import sys
 import unittest
 
-from mock import Mock, patch
+import mock
 from nose.tools import *
+
+sys.path.insert(0, os.path.abspath(".."))
+import forced_dater
 
 class TestForcedDater(object):
 
@@ -14,11 +21,24 @@ class TestForcedDater(object):
             command.append(files)
         output = subprocess.check_output(command)
 
-    def test_setting_dates(self):
-        import os
-        print os.getcwd()
-
+    @mock.patch("forced_dater.plistlib.writePlist")
+    def test_setting_dates(self, mock_plistlib):
         # Run with a properly formed date, but no files.
-        date = "2011-08-03T13:00:00Z"
-        output = self.run_forced_dater(date, [])
+        test_date = "2011-08-03T13:00:00Z"
+        test_datetime = datetime.datetime.strptime(test_date,
+                                                   "%Y-%m-%dT%H:%M:%SZ")
+        output = self.run_forced_dater(test_date, [])
         assert_equals(output, None)
+        # import pdb;pdb.set_trace()
+
+        # Okay-let's try this for real. Update an actual file.
+        sys.argv = ["forced_dater.py", test_date,
+                    "test/resources/Crypt-0.7.2.pkginfo"]
+        forced_dater.main()
+        assert_equal(test_datetime,
+                     mock_plistlib.call_args[0][0]["force_install_after_date"])
+
+
+
+def fakeWritePlist(data_object, path):
+    pass
