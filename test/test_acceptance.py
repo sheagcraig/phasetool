c4aa0c978b1092af01b37cf5ecbd2dca9e9862e6
Shea Craig <sheagcraig@gmail.com>
2015-11-02 21:20:51 -0500

    Clean up tests to be more clear.

--- test/test_acceptance.py
+++ test/test_acceptance.py
@@ -1,3 +1,8 @@
+"""Acceptance tests for forced_dater. Since it seems to be a thing to
+name the user for your user story, we will name our user Leif.
+"""
+
+
 import datetime
 import os
 import plistlib
@@ -8,10 +13,31 @@ import unittest
 import mock
 from nose.tools import *
 
-sys.path.insert(0, os.path.abspath(".."))
 import forced_dater
 
-class TestForcedDater(object):
+class TestForcedDater():
+    """Leif tries to automate his Munki and AutoPkg phase testing."""
+
+    def __init__(self):
+        self.test_date = "2011-08-03T13:00:00Z"
+        self.test_datetime = datetime.datetime.strptime(self.test_date,
+                                                        "%Y-%m-%dT%H:%M:%SZ")
+
+    @mock.patch("forced_dater.plistlib.writePlist")
+    def test_setting_dates(self, mock_plistlib):
+        # First, Leif is a bit uneasy about dumping a big chunk of
+        # pkginfos into this thing. So he tries just a date.
+        output = self.run_forced_dater(self.test_date, [])
+        assert_equals(output, None)
+
+        # Okay-let's try this for real. Leif is going to update an
+        # actual file.
+        sys.argv = ["forced_dater.py", self.test_date,
+                    "test/resources/Crypt-0.7.2.pkginfo"]
+        forced_dater.main()
+        # import pdb;pdb.set_trace()
+        assert_equal(self.test_datetime,
+                     mock_plistlib.call_args[0][0]["force_install_after_date"])
 
     def run_forced_dater(self, date, files):
         command = ["python", "forced_dater.py", date]
@@ -20,25 +46,3 @@ class TestForcedDater(object):
         else:
             command.append(files)
         output = subprocess.check_output(command)
-
-    @mock.patch("forced_dater.plistlib.writePlist")
-    def test_setting_dates(self, mock_plistlib):
-        # Run with a properly formed date, but no files.
-        test_date = "2011-08-03T13:00:00Z"
-        test_datetime = datetime.datetime.strptime(test_date,
-                                                   "%Y-%m-%dT%H:%M:%SZ")
-        output = self.run_forced_dater(test_date, [])
-        assert_equals(output, None)
-        # import pdb;pdb.set_trace()
-
-        # Okay-let's try this for real. Update an actual file.
-        sys.argv = ["forced_dater.py", test_date,
-                    "test/resources/Crypt-0.7.2.pkginfo"]
-        forced_dater.main()
-        assert_equal(test_datetime,
-                     mock_plistlib.call_args[0][0]["force_install_after_date"])
-
-
-
-def fakeWritePlist(data_object, path):
-    pass
